4f8fed6ad9e9257897d61b249a3d36723d44f3cb
Xiaoyeliang <877283271@qq.com>
2025-05-19 13:23:17 +0800

    11

---
+++ FineUICore.EmptyProject.RazorForms/Pages/OPBasic/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FineUICore.EmptyProject.RazorForms.Pages.OPBasic
+{
+    public partial class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
